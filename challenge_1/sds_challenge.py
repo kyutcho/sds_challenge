6647c32ca4e2065500d82687fc4d11b92e12a9f6
Jayden Cho - HP Omen <kyutcho@umich.edu>
2020-09-02 14:07:36 -0700

    First commit

---
+++ challenge_1/sds_challenge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 13:38:27 2020
+
+@author: david
+"""
+
+import numpy as np
+import pandas as pd
+
+# file_path = "\challenge_1" 
+df = pd.read_csv("D:\Learning\Projects\sds_challenges\challenge_1\data\public_flights.csv")
+
+print(df.columns)
+
+a = np.array([12,3,5])
