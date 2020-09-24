a1bfaa8fee6869836985b2a1905c54d3d8ac7f20
Jayden Cho - HP Omen <kyutcho@umich.edu>
2020-09-23 18:08:55 -0700

    EDA cancelled variable

--- challenge_1/sds_challenge.py
+++ challenge_1/sds_challenge.py
@@ -8,11 +8,12 @@ Created on Wed Sep  2 13:38:27 2020
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # file_path = "\challenge_1"
 win_path = "D:/Learning/Projects/sds_challenges/challenge_1/data/public_flights.csv"
 mac_path = "~/Projects/sds_challenge/challenge_1/data/public_flights.csv"
-df = pd.read_csv(mac_path)
+df = pd.read_csv(win_path)
 
 print(df.columns)
 
@@ -20,9 +21,15 @@ print(df.info())
 
 # sns.distplot(df['DISTANCE']);
 
-df['CANCELLED'].value_counts()
+# EDA
 
-a = np.array([12, 3, 5])
-b = np.array([3, 4, 5])
+# Cancelled
+df["CANCELLED"].value_counts()[0]
+g = sns.countplot(data = df, x = "CANCELLED")
+print(type(g))
 
-print("This is only for the testing git.")
+plt.annotate(str(df["CANCELLED"].value_counts()[0]), )
+
+sns.catplot(data = df, x = "YEAR", kind = "count")
+df["YEAR"].value_counts()
+
